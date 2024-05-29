12df21490237c07138ec6e827fd110625535a559
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-05-29 15:43:04 -0600

    staging

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import Intro from './components/intro';
-import 
 
-
-
-
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import About from './components/About';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
