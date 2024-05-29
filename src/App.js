a22d833346802b15823971d1fd94034bfe12ac3e
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-05-29 15:42:29 -0600

    env set up more

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Intro from './components/intro';
+import 
+
+
+
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          {/* Add more routes for other pages here */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
