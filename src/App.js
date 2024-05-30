f4a0c188dbe4de4b2804fcadcb8ecd4efb92e090
Chase Olsen <olsenc07@Chases-Laptop.local>
2024-05-29 22:03:06 -0600

    onto staging env

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Intro />
+      {/* add landing */}
         <Routes>
           <Route path="/" element={<Intro />} />
           {/* <Route path="/info" element={<info />} /> */}
---
+++ src/components/intro.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <h1>Welcome to MyMoss!</h1>
+         
+    </div>
+  );
+};
+
+export default Intro;
